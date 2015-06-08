bd14471cf60ebd77c6bc2fc971b712fc86b9fc42
Anita Seagraves <anita.seagraves@compdsn.com>
2015-06-08 14:53:44 -0600

    Initial Commit

---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Anita Seagraves on 6/8/15.
+//  Copyright (c) 2015 Anita Seagraves. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
